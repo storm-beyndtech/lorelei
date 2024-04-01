c2a8373d110fefad234e8b7c87ef6755796c018d
khameleonstorm <khameleonstorm@gmail.com>
2024-04-01 23:40:45 +0100

    env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    SMTP_USER: 'help@loreleielisabeth.com',
-    SMTP_PASSWORD: '3099545689'
-  },
 }
 module.exports = nextConfig
